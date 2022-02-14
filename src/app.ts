62571ea2c33b2b4714ec26a4b52a9a7e00206de1
adco23 <adco2393@gmail.com>
2022-02-14 11:51:12 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -24,16 +24,16 @@ function ignoreFavicon(req:any, res:any, next:any) {
   next();
 }
 
-// app.use((req:any, res:any, next:any) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+app.use((req:any, res:any, next:any) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
